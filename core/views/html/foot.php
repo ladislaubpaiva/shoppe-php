85f14e54ef3554c0c6d69fec59cc17a4046e7912
Ladislau Paiva <ladislaubpaiva@hotmail.com>
2022-05-22 12:52:51 +0100

    change title page and icons source

--- core/views/html/foot.php
+++ core/views/html/foot.php
@@ -1,4 +1,5 @@
-<script src="https://unpkg.com/boxicons@2.1.2/dist/boxicons.js"></script>
+<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 </body>
 
 </html>
--- core/views/html/head.php
+++ core/views/html/head.php
@@ -5,17 +5,14 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <?php switch ($title) {
-    case 'Home':
-      echo "<title>Shoppe - Best Products for you</title>";
-      break;
-    default:
-      echo "<title>$title - Shoppe</title>";
-      break;
-  }
-  ?>
-
+  <?php if ($title == 'Home') : ?>
+    <title><?= APP_NAME ?> - Best Products For You</title>
+  <?php else : ?>
+    <title><?= $title . ' - ' . APP_NAME ?></title>
+  <?php endif; ?>
+  <link rel="shortcut icon" href="public/assets/images/favicon.svg" type="image/x-icon">
   <link rel="stylesheet" href="public/assets/styles/<?= lcfirst($title) ?>.css" type="text/css" />
+  <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&display=swap" rel="stylesheet">
 </head>
 
 <body>
